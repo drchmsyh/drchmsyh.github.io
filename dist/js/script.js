2ebc526efdcd22826b0f354e918b062885890e6c
drchmsyh <dirchamsyah@gmail.com>
2023-05-25 02:00:22 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,3 +17,29 @@ window.onscroll = function () {
         header.classList.remove('navbar-fixed');
     }
 };
+
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+})
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+
+}
