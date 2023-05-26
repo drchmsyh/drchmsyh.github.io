714e9161c8828a36ad1a44e783eeee9d0c505dda
drchmsyh <dirchamsyah@gmail.com>
2023-05-26 19:18:15 +0700

    'update'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding: '32px',
+      padding: "32px",
     },
     extend: {
       colors: {
-        primary: '#06b6d4',
-        secondary: '#64748b',
-        dark: '#0f172a',
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
       animation: {
-        text: 'text 5s ease infinite',
+        text: "text 5s ease infinite",
       },
       keyframes: {
         text: {
-          '0%, 100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center',
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
           },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center',
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
           },
         },
       },
     },
   },
   plugins: [],
-}
-
+};
